680ef53a46fa414528997da71c15fc2318705969
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2024-06-07 22:44:18 +0530

    initial commit

---
+++ src/main/java/me/dushminmalisha/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package me.dushminmalisha.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+}
